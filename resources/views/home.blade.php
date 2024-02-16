9575493c7e814c0d3c88403a51e08c6c1a2db3ef
Aurélien Chappard <aurelien.chappard@deefuse.fr>
2024-02-16 14:53:28 +0100

    Templating base

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('title', 'Développeur Laravel, Wordpress et intégrateur responsive')
+@section('content')
+    @include('home.prehome')
+    @include('home.title')
+    @include('home.competences')
+    @include('home.presentation')
+    @include('home.experiences')
+@endsection
+
